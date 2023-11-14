78f58426447ec8ca48b99a2e22919999152059e8
r.marfutin <r.marfutin@sh.dunice.net>
2023-11-14 11:50:22 +0300

    feature:Add simple controller with simple data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,54 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Practice_NPOI.Models;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
 
 namespace Practice_NPOI.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private List<TableModel> GetData()
     {
-        _logger = logger;
+        List<TableModel> data = new List<TableModel>
+        {
+            new TableModel { Field1 = "Value1", Field2 = "Value2" }
+        };
+
+        return data;
     }
 
-    public IActionResult Index()
+    [HttpGet("api/downladexcel")]
+    public IActionResult DownloadExcel()
     {
-        return View();
-    }
+        IWorkbook workbook = new HSSFWorkbook();
+        ISheet sheet = workbook.CreateSheet("История обслуживания");
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        List<TableModel> data = GetData();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        IRow headerRow = sheet.CreateRow(0);
+        for (int i = 0; i < data.Count; i++)
+        {
+            TableModel record = data[i];
+            IRow row = sheet.CreateRow(i + 1);
+            
+            row.CreateCell(0).SetCellValue(record.Field1);
+            row.CreateCell(1).SetCellValue(record.Field2);
+        }
+
+        using (MemoryStream stream = new MemoryStream())
+        {
+            workbook.Write(stream);
+            var content = stream.ToArray();
+
+            return File(content, "application/vnd.ms-excel", "История обслуживания.xls");
+        }
     }
 }
+
+    public class TableModel
+    {
+        public string Field1 { get; set; }
+        public string Field2 { get; set; }
+    }    
+
--- Models/TableModel.cs
+++ Models/TableModel.cs
@@ -1,6 +1,8 @@
-﻿namespace Practice_NPOI.Model;
-
-public class TableModel
-{
-    
-}
+﻿// namespace Practice_NPOI.Model
+// {
+//     public class TableModel
+//     {
+//         public string Field1 { get; set; }
+//         public string Field2 { get; set; }
+//     }    
+// }
